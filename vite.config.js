8431cc043bf1aec35fb4b66cc9d011a8398de146
Lim Chee Aun <cheeaun@gmail.com>
2025-01-08 16:16:38 +0800

    Fix Sonda

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { lingui } from '@lingui/vite-plugin';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
-import { SondaRollupPlugin } from 'sonda';
+import Sonda from 'sonda/vite';
 import { defineConfig, loadEnv } from 'vite';
 import generateFile from 'vite-plugin-generate-file';
 import { run } from 'vite-plugin-run';
@@ -34,7 +34,7 @@ export default defineConfig({
         },
       ],
     }),
-    SondaRollupPlugin({
+    Sonda({
       open: false,
       detailed: true,
       brotli: true,
